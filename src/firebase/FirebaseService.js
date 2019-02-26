0339287f8000bfcf6f84bdb9512573342eb3fef1
Gautier Julien <jululien.gautier@laposte.net>
2019-02-26 11:52:18 +0100

    bordel ya encore des bug mais ça run

--- src/firebase/FirebaseService.js
+++ src/firebase/FirebaseService.js
@@ -1,5 +1,8 @@
-export default class FirebaseService {
-    constructor(firebase) {
+import * as firebase from 'firebase';
+
+class FirebaseService {
+    constructor() {
+        console.log('firebase',firebase);
         var config = {
             apiKey: "AIzaSyA_lrqVKPbCWfCH16JuUrCfmADzIeAlTA4",
             authDomain: "jayt-3fe91.firebaseapp.com",
@@ -12,12 +15,31 @@ export default class FirebaseService {
         this.db = firebase.firestore();
     }
 
-    addUser(data) {
+    addUser(data, callback) {
         return new Promise((resolve, reject) => {
             this.db.collection("users").add(data).then(newData => {
+                // console.log("Document written with ID: ", newData.id,newData.get());
                 data.id = newData.id;
                 resolve(data);
             });
         })
     }
+
+    getUsers() {
+        return new Promise((resolve, reject) => {
+            let out = [];
+            this.db.collection("users").get().then(querySnapshot => {
+                querySnapshot.forEach(function (doc) {
+                    let current = doc.data();
+                    current.id = doc.id;
+                    out.push(current);
+                    // // doc.data() is never undefined for query doc snapshots
+                    // console.log(doc.id, " => ", doc.data());
+                });
+                resolve(out);
+            });
+        })
+    }
 }
+
+export default FirebaseService;
