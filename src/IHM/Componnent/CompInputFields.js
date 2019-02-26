5908fa0ef7062a48ed36a03a32f810f3715f9fd8
Alexandre <a.rabreau96@gmail.com>
2019-02-26 11:53:25 +0100

    Test du git ignore

--- src/IHM/Componnent/CompInputFields.js
+++ src/IHM/Componnent/CompInputFields.js
@@ -1,1 +1,8 @@
 //Champs de base pour SAO
+export default class CompInputFields extends HTMLElement {
+    constructor(){
+        super();
+    }
+}
+
+window.customElements.define('InputFields',CompInputFields);
